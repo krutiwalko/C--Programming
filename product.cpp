ca50afd527aaeb313cb10e9b1c145d4d8bfeaf11
krutiwalko <69305743+krutiwalko@users.noreply.github.com>
2021-08-10 15:24:12 +0530

    Create product.cpp

---
+++ product.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	float a,b;
+	cout<<"Enter two numbers separated by space:";
+	cin>>a>>b;
+	cout<<"The product of " << a << " and "<<b<< " is:"<<a*b;
+	return 0;
+}
