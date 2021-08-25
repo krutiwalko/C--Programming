61536daae4f8da2f823bee40ac883329dd332017
krutiwalko <69305743+krutiwalko@users.noreply.github.com>
2021-08-25 18:14:25 +0530

    array_pointer.cc

---
+++ array_pointer.cc
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cout<<"Enter the size of array: ";
+	cin>>n;
+	int arr[n];
+	int *p=arr;
+	for(int i=0;i<n;i++){
+		cin>>*p;
+		p++;
+	}
+	p=arr;
+	for(int j=0; j<n; j++){
+		cout<<*p<<" ";
+		p++;
+	}
+	return 0;
+}
